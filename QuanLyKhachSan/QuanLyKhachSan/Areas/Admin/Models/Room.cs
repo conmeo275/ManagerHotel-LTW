528a4ff6ab14974cc65142f2d3261f603e6fc1b9
conmeo275 <danganhhaivl@gmail.com>
2020-05-03 16:31:56 +0700

    Fist commit

---
+++ QuanLyKhachSan/QuanLyKhachSan/Areas/Admin/Models/Room.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace QuanLyKhachSan.Areas.Admin.Models
+{
+    public class Room
+    {
+        public int Id { get; set; }
+        public string RoomType { get; set; }
+        public float RoomPrice { get; set; }
+        public string RoomPic { get; set; }
+    }
+}
